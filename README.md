0d974ebadaf832bdaa7ef2cb4b8ac2a7ec53fb41
Sam Power <samp25@gmail.com>
2024-03-04 07:04:36 +0000

    uk-postcode-validator - amended readme and main files

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# uk-postcode-validator
+# UK Postcode Validator
+A simple script that validates and formats a UK postcode
+
+An example of how you could run the code might be:
+
+```python
+from main import format_uk_postcode, validate_uk_postcode
+postcode = 'aa9a9aa'
+print(f'{postcode} formated to {format_uk_postcode(postcode)}')
+print(f'{postcode} returned {validate_uk_postcode(postcode)} as a valid postcode')
+print(f'foobar returned {validate_uk_postcode("foobar")} as a valid postcode')
+```
+Returns the following:
+```
+aa9a9aa formated to AA9A 9AA
+aa9a9aa returned True as a valid postcode
+foobar returned False as a valid postcode
+```
+
+Tests can be run with following in a terminal window:
+
+```bash
+python test_main.py
+```
--- main.py
+++ main.py
@@ -1,29 +1,24 @@
 import re
 
 # TODO update README.md
-# TODO make requirments.txt
 
 def format_uk_postcode(postcode):
-    # remove any non alphanumeric characters
-    postcode = "".join(character for character in postcode if character.isalnum())
-
-    # last 4 characters are always a space, followed by a digit, followed two letters
+    postcode = ''.join(character for character in postcode if character.isalnum())
     postcode = f'{postcode[:-3]} {postcode[-3:]}'
 
-    # TODO should this validate post code?
     return postcode.upper()
 
 def validate_uk_postcode(postcode):
     # regex taken from https://en.wikipedia.org/wiki/Postcodes_in_the_United_Kingdom#Formatting
-    regex = r"^(([A-Z]{1,2}[0-9][A-Z0-9]?|ASCN|STHL|TDCU|BBND|[BFS]IQQ|PCRN|TKCA) ?[0-9][A-Z]{2}|BFPO ?[0-9]{1,4}|(KY[0-9]|MSR|VG|AI)[ -]?[0-9]{4}|[A-Z]{2} ?[0-9]{2}|GE ?CX|GIR ?0A{2}|SAN ?TA1)$"
+    regex = r'^(([A-Z]{1,2}[0-9][A-Z0-9]?|ASCN|STHL|TDCU|BBND|[BFS]IQQ|PCRN|TKCA) ?[0-9][A-Z]{2}|BFPO ?[0-9]{1,4}|(KY[0-9]|MSR|VG|AI)[ -]?[0-9]{4}|[A-Z]{2} ?[0-9]{2}|GE ?CX|GIR ?0A{2}|SAN ?TA1)$'
     
     if re.match(regex, postcode, re.IGNORECASE):
         return True
     else:
         return False
 
-if __name__ == "__main__":
-    postcode='sw1w0ny'
-    print(f"{postcode} formated to {format_uk_postcode(postcode)}")
-    print(f"{postcode} returned {validate_uk_postcode(postcode)} as a valid postcode")
-    print(f"foobar returned {validate_uk_postcode('foobar')} as a valid postcode")
+if __name__ == '__main__':
+    postcode = 'aa9a9aa'
+    print(f'{postcode} formated to {format_uk_postcode(postcode)}')
+    print(f'{postcode} returned {validate_uk_postcode(postcode)} as a valid postcode')
+    print(f'foobar returned {validate_uk_postcode("foobar")} as a valid postcode')
